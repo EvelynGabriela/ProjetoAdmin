4f5f78722e526b5ab412ba933f3ef4f8fd535e75
EvelynGabriela <gabi_evelyn@hotmail.com>
2017-02-21 13:21:29 -0300

    admin

---
+++ script.js
@@ -0,0 +1,16 @@
+	/*function Login(){
+		var usr=0;
+		var usuario=document.login-inputs.usuario.value;
+			usuario=usuario.toLowerCase();
+		var senha=document.login-inputs.senha.value;
+			senha=senha.toLowerCase();
+		if (usuario=="admin" && senha=="admin") {
+			window.location="CAMINHO BANCO"; done=1;
+		}
+		if (usr==0) { alert("Dados incorretos, tente novamente"); }
+	}
+
+*/
+function click() {
+    window.open('../Projeto/quemsomos.html', 'button1');
+}
